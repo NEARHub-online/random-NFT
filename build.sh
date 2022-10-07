8743d087e62467d52d9ef091de57e0c477842a93
Marco Torello <marcotorello@gmail.com>
2022-10-07 13:48:08 +0200

    first commit

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -e
 rustup target add wasm32-unknown-unknown
 cd "`dirname $0`"
 source flags.sh
-cargo build --all --target wasm32-unknown-unknown --release
+RUSTFLAGS='-C link-arg=-s' cargo build --all --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/*.wasm ./res/
